6a880ebcc4fa1da6b9b326dea3f2c347d6c69d07
cemujax <cemujax@gmail.com>
2019-03-03 00:58:18 +0900

    NotFound  페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/views/Home'
+import NotFound from '@/views/NotFound'
 
 Vue.use(Router)
 
@@ -11,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '*',
+      component: NotFound
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <NavToolbar/>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout>
+          <v-flex xs12 sm8 offset-sm2>
+            <h2>{{msg}}</h2>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import NavToolbar from "@/components/common/NavToolbar.vue";
+import Footer from "@/components/common/Footer.vue";
+
+export default {
+  name: "NotFound",
+  components: { NavToolbar, Footer },
+  data() {
+    return {
+      msg: "Not Found"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
