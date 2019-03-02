552741188f799b98407e9c2a392aa5a8f8342650
cemujax <cemujax@gmail.com>
2019-03-03 00:50:32 +0900

    네비게이션툴바 푸터 추가

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer color="blue darken-1" app>
+    <v-layout justify-center row wrap>
+      <v-flex blue darken-2 py-3 text-xs-center white--text xs12>
+        &copy;2019
+        <strong>cemujax</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/common/NavToolbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" fixed app>
+      <v-list dense></v-list>
+    </v-navigation-drawer>
+
+    <!-- toolbar -->
+    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-1" dark app fixed>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">
+          <router-link to="/">My Booket</router-link>
+        </span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn flat>로그인</v-btn>
+      <v-btn flat>회원가입</v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null
+    };
+  }
+};
+</script>
+
+<style>
+</style>
