8983a34076e82e9f04c10f16b40282332dc6d270
cemujax <cemujax@gmail.com>
2019-03-03 20:10:38 +0900

    api 추가

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const REST_API = "https://reqres.in/api";
+
+const request = (method, url, data) => {
+  return axios({
+    method,
+    url: REST_API + url,
+    data
+  }).then(result => {
+    return result;
+  });
+};
+
+export const auth = {
+  login(email, password) {
+    return request("post", "/login", { email, password });
+  }
+};
