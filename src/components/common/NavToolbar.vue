f511fbe1e0b2464f1a07dfbe9cca673cda6329ed
cemujax <cemujax@gmail.com>
2019-03-03 17:15:09 +0900

    라우터 링크 변경

--- src/components/common/NavToolbar.vue
+++ src/components/common/NavToolbar.vue
@@ -2,36 +2,30 @@
   <div>
     <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" fixed app>
       <v-list dense>
-        <router-link to="/">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>home</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Home</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <router-link to="/booket">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>book</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Booket</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <router-link to="/contacts">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>contact_mail</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Contacts</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
+        <v-list-tile :to="{name: 'home'}">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile :to="{name: 'booket'}">
+          <v-list-tile-action>
+            <v-icon>book</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Booket</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile :to="{name: 'contacts'}">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contacts</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
 
@@ -39,11 +33,11 @@
     <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-1" dark app fixed>
       <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-
         <router-link to="/" style="color: white;">
           <span>My Booket</span>
         </router-link>
       </v-toolbar-title>
+
       <v-text-field
         flat
         solo-inverted
@@ -53,18 +47,12 @@
         class="hidden-sm-and-down"
       ></v-text-field>
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
+
       <v-btn icon>
         <v-icon>notifications</v-icon>
       </v-btn>
-      <router-link to="/login">
-        <v-btn flat>로그인</v-btn>
-      </router-link>
-      <router-link to="/signup">
-        <v-btn flat>회원가입</v-btn>
-      </router-link>
+      <v-btn flat :to="{name: 'login'}">로그인</v-btn>
+      <v-btn flat :to="{name: 'signup'}">회원가입</v-btn>
     </v-toolbar>
   </div>
 </template>
