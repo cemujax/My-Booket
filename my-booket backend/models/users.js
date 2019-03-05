50726d5fa65565bc0b3c7761d213528168cc8f8f
cemujax <cemujax@gmail.com>
2019-03-05 18:59:42 +0900

    로그인 API수정

--- my-booket backend/models/users.js
+++ my-booket backend/models/users.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  confirmFlag: { type: Number, default: 1 }
+  userName: { type: String, required: true, default: "" },
+  confirmFlag: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("Users", usersSchema);
--- my-booket backend/routes/index.js
+++ my-booket backend/routes/index.js
@@ -5,12 +5,17 @@ const router = express.Router();
 
 router.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await Users.findOne({ email, password });
+  const resUser = await Users.findOne({ email, password });
 
-  if (!user) return res.status(401).json({ error: "Login failure" });
-
-  const token = authService.signToken(user.id);
-  res.json({ token, user });
+  if (!resUser) return res.status(401).json({ error: "로그인 실패" });
+  const token = authService.signToken(resUser.id);
+  const { userName, confirmFlag } = resUser;
+  const user = {
+    email,
+    userName,
+    confirmFlag
+  };
+  res.json({ data: { token, user } });
 });
 
 module.exports = router;
