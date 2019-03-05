2c30008b0875637fa6719e3123e8b0d28a226184
cemujax <cemujax@gmail.com>
2019-03-05 23:50:45 +0900

    인터파크 신간도서 API 구현

---
+++ my-booket backend/routes/interpark.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+const INTERPARK_KEY = process.env.INTERPARK_KEY || "";
+const DOMAIN = "http://book.interpark.com/api";
+const NEWBOOK_API = "/newBook.api";
+
+// 신간도서 조회
+router.get(NEWBOOK_API, (req, res, next) => {
+  axios
+    .get(DOMAIN + NEWBOOK_API, {
+      params: {
+        key: INTERPARK_KEY,
+        categoryId: 100,
+        output: "json"
+      }
+    })
+    .then(result => {
+      const data = result.data;
+      const { returnCode, returnMessage, item } = data;
+      if (returnCode !== "000") {
+        console.error(
+          `[newBook.api Error] (${new Date().toISOString()}) ==> Code:${returnCode} , ${returnMessage}`
+        );
+      }
+
+      res.json({ item });
+    })
+    .catch(error => {
+      const status = error.response.status;
+      res.json({ error: status });
+    });
+});
+
+module.exports = router;
