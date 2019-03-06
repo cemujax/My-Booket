79072b595d0f2fac2f4ff7f0c6461ebf82433586
cemujax <cemujax@gmail.com>
2019-03-06 16:31:24 +0900

    책소개 페이지

---
+++ my-booket frontend/src/views/BookInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <NavToolbar/>
+    <v-content>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <h2>BookInfo</h2>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import NavToolbar from "@/components/common/NavToolbar.vue";
+import Footer from "@/components/common/Footer.vue";
+
+export default {
+  name: "BookInfo",
+  components: { NavToolbar, Footer },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
