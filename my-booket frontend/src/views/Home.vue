c5ae142a75ffff82854065ba52f6cf1e2fe1c307
cemujax <cemujax@gmail.com>
2019-03-06 15:16:38 +0900

    신간도서 조회

--- my-booket frontend/src/views/Home.vue
+++ my-booket frontend/src/views/Home.vue
@@ -4,59 +4,51 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout>
-          <v-flex xs12 sm8 offset-sm2>
-            <v-card>
-              <h2>베스트셀러</h2>
-              <v-container fluid>
-                <v-layout row wrap>
-                  <v-flex v-for="n in 9" :key="n" xs4>
-                    <v-card flat tile>
-                      <v-img
-                        :src="`https://us.123rf.com/450wm/kropic/kropic1510/kropic151000023/46969887-%ED%9D%B0%EC%83%89-%EC%BB%A4%EB%B2%84%EC%99%80-%ED%95%A8%EA%BB%98-%EC%B1%85%EC%9D%84-%EB%B9%88.jpg?ver=6`"
-                        height="150px"
-                      ></v-img>
-                    </v-card>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
+          <v-flex xs12 sm6 offset-sm3>
+            <h2 v-if="loading">Loading..</h2>
+            <Carousel header-title="신간도서" :items="newBooks"/>
             <v-divider></v-divider>
-            <!-- card2 -->
-            <v-card>
-              <h2>신간</h2>
-              <v-container fluid>
-                <v-layout row wrap>
-                  <v-flex v-for="n in 9" :key="n" xs4>
-                    <v-card flat tile>
-                      <v-img
-                        :src="`https://us.123rf.com/450wm/kropic/kropic1510/kropic151000023/46969887-%ED%9D%B0%EC%83%89-%EC%BB%A4%EB%B2%84%EC%99%80-%ED%95%A8%EA%BB%98-%EC%B1%85%EC%9D%84-%EB%B9%88.jpg?ver=6`"
-                        height="150px"
-                      ></v-img>
-                    </v-card>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
           </v-flex>
         </v-layout>
       </v-container>
     </v-content>
-
     <Footer/>
   </v-app>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 import NavToolbar from "@/components/common/NavToolbar.vue";
 import Footer from "@/components/common/Footer.vue";
+import Carousel from "@/components/Carousel.vue";
 
 export default {
   name: "Home",
-  components: { NavToolbar, Footer },
+  components: { NavToolbar, Footer, Carousel },
   data() {
     return {
-      msg: "Home Page"
+      msg: "Home Page",
+      loading: false
     };
+  },
+  computed: {
+    ...mapState({
+      newBooks: "newBooks"
+    })
+  },
+  created() {
+    if (!this.$store.state.newBooks.length) this.fetchData();
+  },
+
+  methods: {
+    ...mapActions(["FETCH_NEWBOOKS"]),
+
+    fetchData() {
+      this.loading = true;
+      this.FETCH_NEWBOOKS().finally(_ => {
+        this.loading = false;
+      });
+    }
   }
 };
 </script>
