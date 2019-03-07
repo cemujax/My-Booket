91b663863e2971e3ec2ea9fd650f427f2f70071a
cemujax <cemujax@gmail.com>
2019-03-07 11:41:11 +0900

    베스트셀러 조회 API

--- my-booket backend/routes/interpark.js
+++ my-booket backend/routes/interpark.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const INTERPARK_KEY = process.env.INTERPARK_KEY || "";
 const DOMAIN = "http://book.interpark.com/api";
 const NEWBOOK_API = "/newBook.api";
+const BESTSELLER_API = "/bestSeller.api";
 
 // 신간도서 조회
 router.get(NEWBOOK_API, (req, res, next) => {
@@ -33,4 +34,29 @@ router.get(NEWBOOK_API, (req, res, next) => {
     });
 });
 
+// 베스트셀러 조회
+router.get(BESTSELLER_API, (req, res, next) => {
+  axios
+    .get(DOMAIN + BESTSELLER_API, {
+      params: {
+        key: INTERPARK_KEY,
+        categoryId: 100,
+        output: "json"
+      }
+    })
+    .then(result => {
+      const data = result.data;
+      const { returnCode, returnMessage, item } = data;
+      if (returnCode !== "000") {
+        console.error(
+          `[newBook.api Error] (${new Date().toISOString()}) ==> Code:${returnCode} , ${returnMessage}`
+        );
+      }
+      res.json({ item });
+    })
+    .catch(error => {
+      const status = error.response.status;
+      res.json({ error: status });
+    });
+});
 module.exports = router;
